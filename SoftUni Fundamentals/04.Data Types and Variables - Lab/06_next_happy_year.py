575b769c2edbf122bcc2048db7d4b2de43ba0280
didarata <98163219+didarata@users.noreply.github.com>
2022-05-26 17:36:39 +0300

    Data Types and Variables - Lab Exercises 6 100/100 update

--- SoftUni Fundamentals/04.Data Types and Variables - Lab/06_next_happy_year.py
+++ SoftUni Fundamentals/04.Data Types and Variables - Lab/06_next_happy_year.py
@@ -1,18 +1,13 @@
-number = int(input())
-current_year = []
-for year in range(number, 99999):
-    current_year.append(year)
-    year_str = str(current_year)
-    first_digit = year_str[1]
-    second_digit = year_str[2]
-    third_digit = year_str[3]
-    fourth_digit = year_str[4]
-    fifth_digit = year_str[5]
-    if first_digit != second_digit and first_digit != third_digit and first_digit != fourth_digit and \
-            first_digit != fifth_digit:
-        if second_digit != third_digit and second_digit != fourth_digit and second_digit != fifth_digit:
-            if third_digit != fourth_digit and third_digit != fifth_digit:
-                if fourth_digit != fifth_digit:
-                    print(year)
-                    break
-    current_year = []
+year = int(input())
+happy_year_condition = False
+
+while not happy_year_condition:
+    year += 1
+    set_year = set()
+
+    for i in range(len(str(year))):
+        set_year.add(str(year)[i])
+
+    happy_year_condition = len(set_year) == len(str(year))
+
+print(year)
