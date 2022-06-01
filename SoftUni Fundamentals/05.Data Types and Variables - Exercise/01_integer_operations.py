aef4ed67a9b9bc5260ea1c8c54688738b48f09e4
didarata <98163219+didarata@users.noreply.github.com>
2022-06-01 23:39:36 +0300

    Data Types and Variables - Exercise update

---
+++ SoftUni Fundamentals/05.Data Types and Variables - Exercise/01_integer_operations.py
@@ -0,0 +1,6 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+four_number = int(input())
+final_sum = int((first_number + second_number) / third_number) * four_number
+print(int(final_sum))
