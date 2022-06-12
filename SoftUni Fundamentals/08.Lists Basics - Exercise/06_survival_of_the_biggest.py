416a198fe4dfb5ca5cd825f2b3713c56a0b51368
didarata <chukolna@gmail.com>
2022-06-13 00:59:59 +0300

    list basics exercise tanks 6 completed, working on task 7

--- SoftUni Fundamentals/08.Lists Basics - Exercise/06_survival_of_the_biggest.py
+++ SoftUni Fundamentals/08.Lists Basics - Exercise/06_survival_of_the_biggest.py
@@ -1,8 +1,12 @@
-def remove_numbers(remove_nums):
-    pass
+def remove_numbers(digits, remove):
+    final_list = digits
+    for index in range(remove):
+        final_list.remove(min(final_list))
+    return final_list
 
 
 list_of_integers = input().split()
+integers_to_remove = int(input())
 list_of_digits = [int(i) for i in list_of_integers]
-list_of_digits.sort()
-print(list_of_digits)
+final_print = remove_numbers(list_of_digits, integers_to_remove)
+print(*final_print, sep=", ")
---
+++ SoftUni Fundamentals/08.Lists Basics - Exercise/07_easter_gifts.py
@@ -0,0 +1,6 @@
+name_of_the_gifts = input().split()
+until_no_money = input().split()
+
+while "No Money" not in until_no_money:
+    print('sex')
+
