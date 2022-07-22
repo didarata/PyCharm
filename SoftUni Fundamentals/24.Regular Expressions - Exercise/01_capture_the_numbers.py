f11a5e676b0ffdf5129951abc57325db572bb782
didarata <98163219+didarata@users.noreply.github.com>
2022-07-23 02:25:28 +0300

    Regular Exprecion Exercise 6/6 all done.

---
+++ SoftUni Fundamentals/24.Regular Expressions - Exercise/01_capture_the_numbers.py
@@ -0,0 +1,13 @@
+import re
+
+pattern = '\d+'
+line = input()
+while True:
+    if line:
+        matches = re.findall(pattern, line)
+        if matches:
+            print(' '.join(matches), end=" ")
+        line = input()
+    else:
+        break
+
---
+++ SoftUni Fundamentals/24.Regular Expressions - Exercise/06_extract_the_links.py
@@ -0,0 +1,26 @@
+import re
+
+valid_urls = []
+pattern = '((w{3})\.[A-Za-z0-9]+(\-[A-Za-z0-9]+)*(\.[a-z]+)+)'
+sentence = input()
+while sentence:
+    matches = re.search(pattern, sentence)
+    if matches:
+        valid_urls.append(matches.group(0))
+    sentence = input()
+for valid_url in valid_urls:
+    print(valid_url)
+
+
+# import re
+#
+# valid_urls = []
+# pattern = '((w{3})\.[A-Za-z0-9]+(\-[A-Za-z0-9]+)*(\.[a-z]+)+)'
+# sentence = input()
+# while sentence:
+#     matches = re.finditer(pattern, sentence)
+#     for match in matches:
+#         valid_urls.append(match.group(1))
+#     sentence = input()
+# for valid_url in valid_urls:
+#     print(valid_url)
