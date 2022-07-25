2c1ded93476fe201a082525da3273b3dedcc1806
didarata <98163219+didarata@users.noreply.github.com>
2022-07-25 23:19:46 +0300

    Final Exam preparation working on 02. Emoji Detector. 80/100 so far, regex will not take me down!

---
+++ SoftUni Fundamentals/Final_Exam/02_emoji_detector.py
@@ -0,0 +1,32 @@
+import re
+
+text = input()
+pattern = r"(::|\*\*)([A-Z][a-z]{2,})\1"
+pattern_threshold = r"[0-9]"
+matches = re.finditer(pattern, text)
+matches_threshold = re.findall(pattern_threshold, text)
+
+cool_emojis = {}
+threshold = 0
+
+for digit in matches_threshold:
+    if threshold == 0:
+        threshold += int(digit)
+    else:
+        threshold *= int(digit)
+
+current_count = 0
+
+for match in matches:
+    for word in match.groups():
+        if word.isalpha():
+            for letter in word:
+                current_count += ord(letter)
+            cool_emojis[match.group()] = current_count
+            current_count = 0
+
+print(f'Cool threshold: {threshold}')
+print(f'{len(cool_emojis)} emojis found in the text. The cool ones are:')
+for key, values in cool_emojis.items():
+    if values >= threshold:
+        print(key)
