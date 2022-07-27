ccb5f1e13d720cc19a7026c230406aebfeae95ee
didarata <98163219+didarata@users.noreply.github.com>
2022-07-27 20:14:16 +0300

    Final Exam preparation working on 03, dictionaries are still a weak point. Will have to put some extra effort in next few days.

---
+++ SoftUni Fundamentals/Final_Exam/03_pirates.py
@@ -0,0 +1,23 @@
+command = input().split("||")
+
+targeted_cities = {}
+
+
+while command[0] != "Sail":
+    city, population, gold = command
+    if command[0] not in targeted_cities:
+        targeted_cities[city] = population, gold
+        targeted_cities.update({targeted_cities[city]: "White"})
+    else:
+        targeted_cities.update("baba", "gaga")
+
+    command = input().split("||")
+
+
+
+command = input().split("=>")
+
+while command[0] != "End":
+    pass
+
+    command = input().split("=>")
