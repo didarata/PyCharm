b3aef5ea81032cf7dee49b7cdf5a4c26f14b7965
didarata <98163219+didarata@users.noreply.github.com>
2022-08-05 22:45:32 +0300

    Working on Softuni Fundamentals Django Project Phonebook. Creating new contact

--- SoftUni Fundamentals/Django_Project/djangoproject/phonebook/views.py
+++ SoftUni Fundamentals/Django_Project/djangoproject/phonebook/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, redirect
+from djangoproject.phonebook.models import Contact
+
+def create_contact(request):
+    name = request.POST.get('name')
+    number = request.POST.get('number')
+    contact = Contact(
+        name=name,
+        number=number,
+    )
+    contact.save()
+    return redirect('landing-page')
 
 
 def landing_page(request):
     return render(request, 'phonebook/index.html')
-
