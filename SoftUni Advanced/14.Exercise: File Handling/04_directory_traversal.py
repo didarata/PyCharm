141dd08c84dfb744965266e4cc6dc7001dbfc906
didarata <chukolna@gmail.com>
2022-10-05 23:16:10 +0300

    Python Advanced messing around

--- SoftUni Advanced/14.Exercise: File Handling/04_directory_traversal.py
+++ SoftUni Advanced/14.Exercise: File Handling/04_directory_traversal.py
@@ -1,39 +1,20 @@
 import os
 
 # First decision with Recursive Function:
-#
-# def directory_traversal(path, files):
-#     for el in os.listdir(path):
-#         if os.path.isdir(os.path.join(path, el)):
-#             directory_traversal(os.path.join(path, el), files)
-#         else:
-#             extension = el.split('.')[-1]
-#             if extension not in files:
-#                 files[extension] = []
-#             files[extension].append(el)
-#
-#
-# result = {}
-# directory_traversal('./', result)
-# sorted_result = sorted(result)
-#
-# with open('./report.txt', 'w') as file:
-#     for key in sorted_result:
-#         file.write(f'.{key}\n')
-#         for element in sorted(result[key]):
-#             file.write(f'- - - {element}\n')
+
+def directory_traversal(path, files):
+    for el in os.listdir(path):
+        if os.path.isdir(os.path.join(path, el)):
+            directory_traversal(os.path.join(path, el), files)
+        else:
+            extension = el.split('.')[-1]
+            if extension not in files:
+                files[extension] = []
+            files[extension].append(el)
 
 
-# Second decision with Os.Walk Method:
 result = {}
-
-for _, _, files in os.walk('.'):
-    for file in files:
-        extension = file.split('.')[-1]
-        if extension not in result:
-            result[extension] = []
-        result[extension].append(file)
-
+directory_traversal('./', result)
 sorted_result = sorted(result)
 
 with open('./report.txt', 'w') as file:
@@ -41,3 +22,22 @@ with open('./report.txt', 'w') as file:
         file.write(f'.{key}\n')
         for element in sorted(result[key]):
             file.write(f'- - - {element}\n')
+
+
+# Second decision with Os.Walk Method:
+# result = {}
+#
+# for _, _, files in os.walk('.'):
+#     for file in files:
+#         extension = file.split('.')[-1]
+#         if extension not in result:
+#             result[extension] = []
+#         result[extension].append(file)
+#
+# sorted_result = sorted(result)
+#
+# with open('./report.txt', 'w') as file:
+#     for key in sorted_result:
+#         file.write(f'.{key}\n')
+#         for element in sorted(result[key]):
+#             file.write(f'- - - {element}\n')
