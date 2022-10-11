5cad2d694f2ac7dd75e394b02941a4c6b924b806
didarata <chukolna@gmail.com>
2022-10-11 10:52:41 +0300

    Python Advanced Workshop Excersice trying to make github work

--- SoftUni Advanced/11.Error Handling/03.repeat_text.py
+++
@@ -1,7 +0,0 @@
-# try:
-#     text = input()
-#     repetitions = int(input())
-#     result = text * repetitions
-#     print(result)
-# except ValueError:
-#     print('Variable times must be an integer')
