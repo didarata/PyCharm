7ec058d8ea5307b8daa24752118be15f86729454
Deyan Georgiev <ifdeyan@gmail.com>
2022-10-19 18:06:32 +0300

    Python Advanced Exam Prep BIG DAY!

--- SoftUni Advanced/22.Final Exam me/02_martian_explorer.py
+++ SoftUni Advanced/22.Final Exam me/02_martian_explorer.py
@@ -1,15 +1,27 @@
 from collections import deque
-from operator import index
+
 
 def move(direction_, position_):
     if direction_ == 'up':
-        position_[0] -= 1
+        if position_[0] > 0:
+            position_[0] -= 1
+        else:
+            position_[0] = 5
     elif direction_ == 'down':
-        position_[0] += 1
+        if position_[0] < 5:
+            position_[0] += 1
+        else:
+            position_[0] = 0
     elif direction_ == 'left':
-        position_[1] -= 1
+        if position_[1] > 0:
+            position_[1] -= 1
+        else:
+            position_[1] = 5
     elif direction_ == 'right':
-        position_[1] += 1
+        if position_[1] < 5:
+            position_[1] += 1
+        else:
+            position_[1] = 0
     return position_
 
 
@@ -20,16 +32,37 @@ for _ in range(6):
 movement = deque(input().split(", "))
 position = []
 
-for row,j in enumerate(matrix):
-    for col,el in enumerate(j):
+water = 0
+metal = 0
+concrete = 0
+
+for row, j in enumerate(matrix):
+    for col, el in enumerate(j):
         if el == "E":
-            position = row, col
+            position.append(row)
+            position.append(col)
 
 while movement:
     current_command = movement.popleft()
     position = move(current_command, position)
-    for row in matrix:
-        print(' '.join(row))
 
-# for row in matrix:
-#     print(' '.join(row))
+    if matrix[position[0]][position[1]] == "R":
+        print(f"Rover got broken at ({position[0]}, {position[1]})")
+        break
+    elif matrix[position[0]][position[1]] == "W":
+        water += 1
+        print(f"Water deposit found at ({position[0]}, {position[1]})")
+    elif matrix[position[0]][position[1]] == "M":
+        metal += 1
+        print(f"Metal deposit found at ({position[0]}, {position[1]})")
+    elif matrix[position[0]][position[1]] == "C":
+        concrete += 1
+        print(f"Concrete deposit found at ({position[0]}, {position[1]})")
+    elif matrix[position[0]][position[1]] == "-":
+        continue
+
+if water != 0 and metal != 0 and concrete != 0:
+    print("Area suitable to start the colony.")
+else:
+    print("Area not suitable to start the colony.")
+
