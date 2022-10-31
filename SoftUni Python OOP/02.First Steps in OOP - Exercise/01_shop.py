5a2f7426764a620619ace917c60060cc5329a539
Deyan Georgiev <ifdeyan@gmail.com>
2022-10-31 09:47:48 +0200

    Python OOP First Steps in OOP - Exercise

--- SoftUni Python OOP/02.First Steps in OOP - Exercise/01_shop.py
+++ SoftUni Python OOP/02.First Steps in OOP - Exercise/01_shop.py
@@ -1,1 +1,13 @@
-aa
+class Shop:
+    def __init__(self, name, items):
+        self.name = name
+        self.items = items
+
+    def get_items_count(self):
+        return len(self.items)
+
+
+# https://judge.softuni.org/Contests/Compete/Index/1935#0
+
+shop = Shop("My Shop", ["Apples", "Bananas", "Cucumbers"])
+print(shop.get_items_count())
